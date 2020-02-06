9cf5b7ae09ced6c27de65df4d6f69e3661e3f7f3
Margarita Belyavtseva <rbeliavceva@gmail.com>
2020-02-06 13:07:56 +0300

    Убран второй тег body

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -5,7 +5,7 @@ const Main = (props) => {
   const {movieTitle, movieGenre, movieYear} = props;
 
   return (
-    <body>
+    <div>
       <div className="visually-hidden">
         <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
           <symbol id="add" viewBox="0 0 19 20">
@@ -330,7 +330,7 @@ const Main = (props) => {
           </div>
         </footer>
       </div>
-    </body>
+    </div>
   );
 };
 
