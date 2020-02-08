d96c0bed48a8ef63f6532f2ae54338fb2cea01e0
Margarita Belyavtseva <rbeliavceva@gmail.com>
2020-02-08 20:53:49 +0300

    Деструктуризация пропсов в movieCard

--- src/components/movieCard/movieCard.jsx
+++ src/components/movieCard/movieCard.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const MovieCard = (props) => {
+const MovieCard = ({title, className}) => {
   return (
-    <article className={`small-movie-card ` + props.className}>
+    <article className={`small-movie-card ` + className}>
       <div className="small-movie-card__image">
-        <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt={props.title} width="280" height="175" />
+        <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt={title} width="280" height="175" />
       </div>
       <h3 className="small-movie-card__title">
-        <a className="small-movie-card__link" href="movie-page.html">{props.title}</a>
+        <a className="small-movie-card__link" href="movie-page.html">{title}</a>
       </h3>
     </article>
   );
