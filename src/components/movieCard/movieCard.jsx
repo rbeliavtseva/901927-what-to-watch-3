3afaf3eba0af81e09d11d09a7fb34d02837e23ad
Margarita Belyavtseva <rbeliavceva@gmail.com>
2020-02-25 12:55:44 +0300

    Компонент переписан в соответствии с критерием Д12

--- src/components/movieCard/movieCard.jsx
+++ src/components/movieCard/movieCard.jsx
@@ -1,30 +1,25 @@
-import React, {PureComponent} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
 import VideoPlayer from "../videoPlayer/videoPlayer.jsx";
 
+const MovieCard = ({movie, className, onMovieCardClick, isVideoPlaying, handleCardMouseEnter, handleCardMouseLeave}) => {
+  return (
+    <article className={`small-movie-card ` + className}
+      onMouseEnter={handleCardMouseEnter}
+      onMouseLeave={handleCardMouseLeave}>
+      <Link to="/dev-film">
+        <div className="small-movie-card__image" onClick={() => onMovieCardClick(movie.id)}>
+          {isVideoPlaying ? <VideoPlayer movie={movie}/> : <img src={movie.poster} alt={movie.title} width="280" height="175"/>}
+        </div>
+      </Link>
 
-class MovieCard extends PureComponent {
-  render() {
-    const {movie, className, onMovieCardClick, isVideoPlaying, handleCardMouseEnter, handleCardMouseLeave} = this.props;
-
-    return (
-      <article className={`small-movie-card ` + className}
-        onMouseEnter={handleCardMouseEnter}
-        onMouseLeave={handleCardMouseLeave}>
-        <Link to="/dev-film">
-          <div className="small-movie-card__image" onClick={() => onMovieCardClick(movie.id)}>
-            {isVideoPlaying ? <VideoPlayer movie={movie}/> : <img src={movie.poster} alt={movie.title} width="280" height="175"/>}
-          </div>
-        </Link>
-
-        <h3 className="small-movie-card__title" onClick={() => onMovieCardClick(movie.id)}>
-          <Link to="/dev-film" className="small-movie-card__link">{movie.title}</Link>
-        </h3>
-      </article>
-    );
-  }
-}
+      <h3 className="small-movie-card__title" onClick={() => onMovieCardClick(movie.id)}>
+        <Link to="/dev-film" className="small-movie-card__link">{movie.title}</Link>
+      </h3>
+    </article>
+  );
+};
 
 MovieCard.propTypes = {
   movie: PropTypes.exact({
