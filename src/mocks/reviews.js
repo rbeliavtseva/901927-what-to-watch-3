424bba4add9281885d875f18056aaf64a87f5886
Margarita Belyavtseva <rbeliavceva@gmail.com>
2020-02-28 14:34:52 +0300

    В моки reviews добавлены id

--- src/mocks/reviews.js
+++ src/mocks/reviews.js
@@ -3,36 +3,42 @@ export const reviews = [
     name: `Kate Muir`,
     text: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.`,
     date: `February 27, 2020`,
-    rating: `8,9`
+    rating: `8,9`,
+    id: 1
   },
   {
     name: `Bill Goodykoontz`,
     text: `Ut ornare lectus sit amet. Molestie at elementum eu facilisis sed odio morbi quis. Sit amet dictum sit amet justo donec. Pulvinar neque laoreet suspendisse interdum consectetur libero id faucibus nisl.`,
     date: `February 26, 2020`,
-    rating: `8,0`
+    rating: `8,0`,
+    id: 2
   },
   {
     name: `Amanda Greever`,
     text: `Pulvinar elementum integer enim neque volutpat ac tincidunt. Parturient montes nascetur ridiculus mus mauris vitae.`,
     date: `February 25, 2020`,
-    rating: `8,0`
+    rating: `8,0`,
+    id: 3
   },
   {
     name: `Matthew Lickona`,
     text: `Senectus et netus et malesuada fames ac. Facilisis gravida neque convallis a cras semper auctor. Nulla malesuada pellentesque elit eget. Mattis vulputate enim nulla aliquet.`,
     date: `February 24, 2020`,
-    rating: `7,2`
+    rating: `7,2`,
+    id: 4
   },
   {
     name: `Paula Fleri-Soler`,
     text: `Venenatis urna cursus eget nunc scelerisque viverra mauris in aliquam. Elementum sagittis vitae et leo. In fermentum posuere urna nec tincidunt praesent. Adipiscing elit ut aliquam purus sit amet luctus venenatis.`,
     date: `February 23, 2020`,
-    rating: `7,6`
+    rating: `7,6`,
+    id: 5
   },
   {
     name: `Anna Smith`,
     text: `Mollis nunc sed id semper. Amet consectetur adipiscing elit duis tristique. Iaculis at erat pellentesque adipiscing commodo elit at.`,
     date: `February 22, 2020`,
-    rating: `7,0`
+    rating: `7,0`,
+    id: 6
   }
 ];
