51f2e8229061ac86701e9ff88c37e5e6deabef75
Margarita Belyavtseva <rbeliavceva@gmail.com>
2020-03-23 12:43:53 +0300

    Добавлена иконка play, реализована смена иконок при запуске видео

--- src/components/videoPlayerFullScreen/videoPlayerFullScreen.jsx
+++ src/components/videoPlayerFullScreen/videoPlayerFullScreen.jsx
@@ -88,10 +88,21 @@ class VideoPlayerFullScreen extends React.PureComponent {
 
             <div className="player__controls-row">
               <button type="button" className="player__play" onClick={this.handleVideoStartPause}>
-                <svg viewBox="0 0 19 19" width="19" height="19">
-                  <use xlinkHref="#play-s"></use>
-                </svg>
-                <span>Play</span>
+                {this.state.videoStarted ?
+                  <React.Fragment>
+                    <svg viewBox="0 0 14 21" width="14" height="21">
+                      <use xlinkHref="#pause"></use>
+                    </svg>
+                    <span>Pause</span>
+                  </React.Fragment> :
+                  <React.Fragment>
+                    <svg viewBox="0 0 19 19" width="19" height="19">
+                      <use xlinkHref="#play-s"></use>
+                    </svg>
+                    <span>Play</span>
+                  </React.Fragment>
+                }
+
               </button>
               <div className="player__name">{movie.title}</div>
 
